c51abb14d17773451ae3838fcba6dba7f37a902a
cpakaylonsmith <48833437+cpakaylonsmith@users.noreply.github.com>
2019-05-03 11:04:08 -0500

    finalsave

--- Project/Form2.Designer.cs
+++ Project/Form2.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 242);
+            this.button1.Location = new System.Drawing.Point(237, 242);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -54,46 +54,13 @@
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(122, 47);
+            this.groupBox1.Location = new System.Drawing.Point(12, 47);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(446, 189);
+            this.groupBox1.Size = new System.Drawing.Size(556, 189);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Choose the correct response";
             this.groupBox1.Visible = false;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(40, 32);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(40, 70);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(40, 110);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
             // 
@@ -106,10 +73,43 @@
             this.radioButton4.Text = "radioButton4";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(40, 110);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(40, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(40, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 20);
+            this.label1.Location = new System.Drawing.Point(197, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(156, 13);
             this.label1.TabIndex = 4;
--- Project/Form2.cs
+++ Project/Form2.cs
@@ -12,8 +12,10 @@ namespace Project
 {
     public partial class Form2 : Form
     {
-        question[] questions = new question[1];
+        question[] questions = new question[10];
         DataClasses1DataContext db = new DataClasses1DataContext();
+        Random rand = new Random();
+        int curr = 0, numcorrect = 0; 
         public Form2()
         {
             InitializeComponent();
@@ -22,21 +24,90 @@ namespace Project
         {
             var query =
                 from question in db.questions
-                where question.Id == 1
                 select question;
-            questions[0] = query.First();
+            int numResults = query.Count();
+
+            for (int i = 0; i < questions.Length; i++)
+            {
+                int pos = rand.Next(numResults);
+                while(Array.Exists(questions, element  => element != null && element.Id == pos + 1))
+                {
+                    pos = rand.Next(numResults);
+                }
+                questions[i] = query.ToList().ElementAt(pos);
+            } 
         }
         private void nextQuestion()
         {
-            label1.Text = questions[0].question;
-            radioButton1.Text = questions[0].ans
+            label1.Text = questions[curr].question1;
+            radioButton1.Text = questions[curr].correct_answer;
+            radioButton2.Text = questions[curr].ans;
+            radioButton3.Text = questions[curr].ans2;
+            radioButton4.Text = questions[curr].ans3;
+            RadioButton[] rbs = groupBox1.Controls.OfType<RadioButton>().ToArray();
+
+            for (int i = 0; i < rbs.Length; i++)
+            {
+                string tmp = rbs[i].Text;
+                int num = rand.Next(rbs.Length);
+                rbs[i].Text = rbs[num].Text;
+                rbs[num].Text = tmp;
+
+            }
+            curr++;
+         
+
+        }
+        private void checkQuestions()
+        {
+            RadioButton rb = groupBox1.Controls.OfType<RadioButton>().FirstOrDefault(element => element.Checked);
+            if (rb != null)
+            {
+                if (rb.Text == questions[curr - 1].correct_answer)
+                {
+                    numcorrect++;
+                    MessageBox.Show("Your are correct!", "Result", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Your are incorrect!", "Result", MessageBoxButtons.OK);
+                }
+                if (curr == questions.Length)
+                {
+                    MessageBox.Show("You scored a " + (((double)numcorrect / questions.Length) * 100) + "% on the quiz.", "Result", MessageBoxButtons.OK);
+                    this.Close();
+                }
+                else
+                {
+                    rb.Checked = false;
+                    nextQuestion();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must choose a response", "Result", MessageBoxButtons.OK);
+            }
 
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            generateQuiz();
-            nextQuestion();
+            if(curr == 0)
+            {
+                generateQuiz();
+                nextQuestion();
+                groupBox1.Visible = true;
+                button1.Text = "Submit";
+
+            }
+            else if(curr <= questions.Length)
+            {
+                checkQuestions();
+            
+            }
+
         }
+
     }
 }
